6ba1aad477504e44305aa454a6074efded33ff46
Sergio <sergioruza22@gmail.com>
2023-02-28 11:13:21 -0300

    feat: route get /teams/id

--- app/backend/src/api/routes/teamsRoutes.ts
+++ app/backend/src/api/routes/teamsRoutes.ts
@@ -7,5 +7,5 @@ const Teams = new TeamsController(service);
 const router = Router();
 
 router.get('/teams', (req: Request, res: Response) => Teams.getAll(req, res));
-
+router.get('/teams/:id', (req: Request, res: Response) => Teams.getById(req, res));
 export default router;
--- app/backend/src/api/services/TeamsService.ts
+++ app/backend/src/api/services/TeamsService.ts
@@ -8,7 +8,13 @@ export default class TeamsService implements IServiceTeams {
   protected model: ModelStatic<Team> = Team;
   async getAll(): Promise<ITeam[]> {
     const result = this.model.findAll();
-    if (!result) throw new Error();
+    if (!result) throw new Error('Falha ao buscar por todos os times');
+    return result;
+  }
+
+  async getById(id: number): Promise<ITeam> {
+    const result = await this.model.findByPk(id);
+    if (!result) throw new Error('Falha na busca por id');
     return result;
   }
 }
