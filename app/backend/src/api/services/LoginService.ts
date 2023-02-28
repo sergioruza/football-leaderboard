9b8afaf6182cbf1a213260a3605f860e4e78dfcd
Sergio <sergioruza22@gmail.com>
2023-02-28 18:32:47 -0300

    feat: service rules /login

--- app/backend/src/api/services/LoginService.ts
+++ app/backend/src/api/services/LoginService.ts
@@ -1,34 +1,29 @@
 import { ModelStatic } from 'sequelize';
-// import * as bcryp from 'bcryptjs';
+import * as bcryp from 'bcryptjs';
 import generateToken from '../../token/generateToken';
 import { UserToken } from '../../token/ITokenUser';
 // import GenericError from '../../errors/genericError';
 import User from '../../database/models/UserModel';
 import { ILogin, IServiceLogin } from './interfaces/IServiceLogin';
-// import validateLogin from './validation/LoginValidate';
+import validateLogin from './validation/LoginValidate';
+import GenericError from '../../errors/genericError';
 
 export default class Login implements IServiceLogin {
   protected model: ModelStatic<User> = User;
   async login(info: ILogin): Promise<string> {
-    // validateLogin(info);
+    validateLogin(info);
 
     const find = await this.model.findOne({ where: { email: info.email } });
-    const payload: UserToken = {
-      id: find?.id as number,
-      email: find?.email as string,
-      username: find?.email as string,
-    };
-    // const passwordIsValid = bcryp.compare(info.password, find?.password as string, (err, res) => {
-    //   if (res) {
+    if (!find) throw new GenericError('Invalid email or password', 401);
 
-    //     return ;
-    //   }
-    //   if (err) {
-    //     throw new GenericError('Invalid email or password', 401);
-    //   }
-    // });
-    // console.log(passwordIsValid);
+    const payload: UserToken = {
+      id: find.id as number,
+      email: find.email as string,
+      username: find.email as string,
+    };
+
+    const passwordValid = await bcryp.compare(info.password, find.password as string);
+    if (!passwordValid) throw new GenericError('Invalid email or password', 401);
     return generateToken(payload);
-    // if (!find) throw new GenericError('Invalid email or password', 404);
   }
 }
