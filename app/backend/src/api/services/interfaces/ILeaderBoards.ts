2e2f394be5d0b1438e6bb59259a542d94d089126
Sergio <sergioruza22@gmail.com>
2023-03-03 19:12:19 -0300

    tentando desenvolver placar de cassificação

---
+++ app/backend/src/api/services/interfaces/ILeaderBoards.ts
@@ -0,0 +1,11 @@
+export interface ILearderBoard {
+  totalOfPoints(): void
+  totalGames(): void
+  totalWins(): void
+  totaTlies(): void
+  totalLosses(): void
+  teamUtilization(): void
+  calculateGoals(): void
+  teamRanking(): void
+  leaderBoard(): void
+}
